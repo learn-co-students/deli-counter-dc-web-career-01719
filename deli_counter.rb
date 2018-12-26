31b1e19df99621a53559ad023f7eb8513cf81122
jameseclement <jameseclement@gmail.com>
2018-12-26 01:08:29 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  length = katz_deli.length
+  if length == 0 
+    puts "The line is currently empty."
+  else 
+  place = 1
+  string = "The line is currently:"
+    until place > length
+  string.concat(" #{place}. #{katz_deli[place-1]}")
+  place +=1
+  end 
+  puts string
+end 
+end
+
+
+def take_a_number
+  
+end
+
+
+
+def now_serving
+  
+end
+
+
